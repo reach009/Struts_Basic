5f4fd92da72cdbd3a699b0bd73d40779c84da0a8
reach009 <sovanreach.c007@gmail.com>
2021-05-06 23:35:20 +0800

    add validation functionality

---
+++ src/com/tutorialspoint/struts2/Employee.java
@@ -0,0 +1,43 @@
+package com.tutorialspoint.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Employee extends ActionSupport {
+	private String name;
+	private int age;
+
+	@Override
+	public String execute() {
+		return SUCCESS;
+	}
+
+	public String display() {
+		return NONE;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/*
+	 * Uncomment this if we don't use Employee-validation.xml
+	 * 
+	 * @Override public void validate() { if (name == null ||
+	 * name.trim().equals("")) { addFieldError("name", "The name is required"); }
+	 * 
+	 * if (age < 28 || age > 65) { addFieldError("age",
+	 * "Age must be in between 28 and 65"); } }
+	 */
+}
